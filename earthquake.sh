cb88363d8d3cb786d8d626592216a7283fc67625
Silvio Santoriello <silviosanto6605@gmail.com>
2019-12-27 20:43:01 +0100

    the results will now show only 1 time

--- earthquake.sh
+++ earthquake.sh
@@ -13,7 +13,6 @@ grep -w -i "$3" terremoti.txt > terremotifiltrato.txt;
 input="terremotifiltrato.txt"
 while IFS= read -r line
 do
-echo "$line"
 telegram-send "$line"
 done < "$input"
 2>/dev/null
