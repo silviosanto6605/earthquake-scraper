c42878c3c571653d0173137674b301bc5c25e703
Silvio Santoriello <silviosanto6605@gmail.com>
2019-12-28 19:14:14 +0100

    fix

--- earthquake.sh
+++ earthquake.sh
@@ -8,7 +8,7 @@ curl "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:00&endt
 echo Cerco
 grep -w -i -q "$2" terremoti.txt;
 grep -w -i "$2" terremoti.txt > terremotifiltrato.txt;
-awk -F "|" 'BEGIN{OFS="|"}{print $2,$6,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt;
+awk -F "|" 'BEGIN{OFS="|"}{print $2,$10,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt;
 
 input="terremotifiltrato2.txt";
 while IFS= read -r line
