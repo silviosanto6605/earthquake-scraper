49dc640edcb75d3b8065676793f9fb502935e0bf
Silvio Santoriello <silviosanto6605@gmail.com>
2019-12-28 19:12:11 +0100

    fix echo bug

--- earthquake.sh
+++ earthquake.sh
@@ -6,12 +6,14 @@ date=$(date '+%Y-%m-%d')
 curl "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:00&endtime=${date}T22:22:00&format=text" > terremoti.txt;
 
 echo Cerco
-grep -w -i "$2" terremoti.txt;
+grep -w -i -q "$2" terremoti.txt;
 grep -w -i "$2" terremoti.txt > terremotifiltrato.txt;
-awk -F "|" 'BEGIN{OFS="|"}{print $2,$6,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt
+awk -F "|" 'BEGIN{OFS="|"}{print $2,$6,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt;
+
 input="terremotifiltrato2.txt";
 while IFS= read -r line
 do
+echo $line
 telegram-send "$line";
 done < "$input"
 2>/dev/null
