b0d9026fc7ea0aa2c359ce79a530c2f2b55a5cb5
Silvio Santoriello <silviosanto6605@gmail.com>
2019-12-30 23:16:49 +0100

    add verbose and silent flag

--- telegram-earthquake.sh
+++ telegram-earthquake.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 date=$(date '+%Y-%m-%d')
 
-curl "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:00&endtime=${date}T23:59:59&format=text" > terremoti.txt;
+if [[ "$3" = "-v" ]]; then   #verbose curl output
+    curl "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:01&endtime=${date}T23:59:59&format=text" > terremoti.txt;
+    echo Cerco
+    grep -w -i -q "$2" terremoti.txt;
+    grep -w -i "$2" terremoti.txt > terremotifiltrato.txt;
+    input="terremotifiltrato.txt";
+    while IFS= read -r line
+    do
+    echo $line
+    telegram-send "$line";
+    done < "$input"
+    2>/dev/null
+    exit
 
-echo Cerco
-grep -w -i -q "$2" terremoti.txt;
-grep -w -i "$2" terremoti.txt > terremotifiltrato.txt;
-awk -F "|" 'BEGIN{OFS="|"}{print $2,$10,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt;
+else
+    curl -s "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:01&endtime=${date}T23:59:59&format=text" > terremoti.txt;
+    echo Cerco
+    grep -w -i -q "$2" terremoti.txt;
+    grep -w -i "$2" terremoti.txt > terremotifiltrato.txt;
+    awk -F "|" 'BEGIN{OFS="|"}{print $2,$10,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt;
+    input="terremotifiltrato2.txt";
+    while IFS= read -r line
+    do
+    echo $line
+    telegram-send "$line";
+    done < "$input"
+    2>/dev/null
+    exit
 
-input="terremotifiltrato2.txt";
-while IFS= read -r line
-do
-echo $line
-telegram-send "$line";
-done < "$input"
-2>/dev/null
-exit
+fi
