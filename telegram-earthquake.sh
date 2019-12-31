7876a109e85c0c07fab129353e19b95c760c1ce2
Silvio Santoriello <silviosanto6605@gmail.com>
2019-12-31 10:34:59 +0100

    customized end time flag

--- telegram-earthquake.sh
+++ telegram-earthquake.sh
@@ -15,6 +15,22 @@ if [[ "$3" = "-v" ]]; then   #verbose curl output
     2>/dev/null
     exit
 
+elif [[ "$3" = "-e" ]]; then   #customized end time
+    curl -s "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:01&endtime=$4T23:59:59&format=text" > terremoti.txt;
+    echo Cerco
+    grep -w -i -q "$2" terremoti.txt;
+    grep -w -i "$2" terremoti.txt > terremotifiltrato.txt;
+    awk -F "|" 'BEGIN{OFS="|"}{print $2,$10,$11,$12,$13}' terremotifiltrato.txt > terremotifiltrato2.txt;
+    input="terremotifiltrato2.txt";
+    while IFS= read -r line
+    do
+    echo $line
+    telegram-send "$line"
+    done < "$input"
+    2>/dev/null
+    exit
+
+
 else
     curl -s "http://webservices.ingv.it/fdsnws/event/1/query?starttime=$1T00:00:01&endtime=${date}T23:59:59&format=text" > terremoti.txt;
     echo Cerco
